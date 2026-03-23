99d5bea1a179ff26d2b08c471a31bec7df6b7e39
iThing1 <r930324@gmail.com>
2026-03-23 12:37:53 +0900

    [update]함수의 선언과 호출
    함수를 만드는법(선언)과 Main함수에 만든 함수를 호출하는 방법을 알아보자

--- cppBasics/cppBasics/Main.cpp
+++ cppBasics/cppBasics/Main.cpp
@@ -1,7 +1,21 @@
 ﻿#include <iostream>
 
+int Plus(int x, int y)
+{
+	return x + y;
+}
+
+void ShowResult(int result)
+{
+	std::cout << "결과값: " << result << std::endl;
+}
+
 int main()
 {
-	
+	// 함수 호출부
+	Plus(10, 20);
+	int result = Plus(10, 20);
+	ShowResult(result);
+
 	return 0;
 }
