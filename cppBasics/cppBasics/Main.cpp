b9da429929a872ba73ee68dd0649f9ba13169b85
iThing1 <r930324@gmail.com>
2026-03-24 18:09:39 +0900

    주석 정리 및 화면 조정
    콘솔창에 뜨는 글자 간격 / 코드 진행과정을 알 수 있게 주석을 추가하였다

--- cppBasics/cppBasics/Main.cpp
+++ cppBasics/cppBasics/Main.cpp
@@ -1,17 +1,7 @@
 ﻿#include <iostream>
 #include <Windows.h>
 #include <conio.h>
-// 과제4: 간단한 텍스트 게임 만들기
-// 주제 : 각 원소별 상성 시스템 구현
-// 1. 플레이어는 5개의 속성(불, 물, 바람, 대지, 전기)을 가진 캐릭터를 선택하여 게임을 진행한다.
-// 2. 각 속성은 서로 상성 관계를 가지고 있다. 불 > 바람 > 대지 > 전기 > 물 > 불
-// 3. 가위 바위 보 형식을 사용하여 플레이어와 컴퓨터가 대결한다. 
-// 4. 승패는 상성 관계에 따라 결정된다.
-// 5. 명확한 상성관계에 해당하지 않는 경우는 무승부로 처리한다.
-// 6. 게임의 총 라운드 수는 플레이어가 선택할 수 있도록 한다.
-// 7. 플레이어는 자신의 캐릭터의 속성을 선택하고, 컴퓨터는 랜덤으로 속성을 선택한다.
-// 8. 라운드 수만큼 반복한다. 1라운드마다 승패를 출력하고 기록한다.
-// 9. 게임이 끝난 후, 기록된 플레이어의 승리 횟수, 패배 횟수, 무승부 정보와 점수를 출력한다.
+
 using namespace std;
 
 // 승무패 기록과 점수 합산을 위한 전역 변수
@@ -93,7 +83,7 @@ void PlayGame(int rounds)
 		ElementType playerChoice = static_cast<ElementType>(GetPlayerType());
 		ElementType computerChoice = static_cast<ElementType>(rand() % 5);
 		cout << "=> 컴퓨터가 속성 선택 중..." << endl; Sleep(1000);
-		
+		// 선택된 속성 문자열로 변환
 		string playerElement = 
 			(playerChoice == Fire) ? "불" :
 		    (playerChoice == Water) ? "물" :
@@ -107,15 +97,18 @@ void PlayGame(int rounds)
 			(computerChoice == Earth) ? "대지" :
 			(computerChoice == Electric) ? "전기" : "알 수 없음";
 
-		cout << "  [ 플레이어 ] vs " << "[ 컴퓨터 ]  " << endl;
-		cout << "        " << playerElement << "            " << computerElement << "     " << endl;
+		cout << "  [플레이어] vs " << "[컴퓨터]  " << endl;
+		cout << "     " << playerElement << "         " << computerElement << "   " << endl;
 		
 		int result = PlayRockPaperScissors(playerChoice, computerChoice);
-		Sleep(1000);
+		Sleep(500);
 		if (result == 3) wins++;
 		else if (result == 0) losses++;
 		else draws++;
 	}
+	cout << "모든 라운드가 종료되었습니다!" << endl;
+	cout << "잠시 후 결과가 출력됩니다..." << endl;
+	Sleep(2000);
 }
 void ShowTitleScreen()
 {
