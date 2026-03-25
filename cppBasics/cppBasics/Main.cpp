dd83e782c65657242f63375cd23f39deec70d3af
iThing1 <r930324@gmail.com>
2026-03-25 15:17:35 +0900

    [update]초기 다차원 배열 생성 및 초기화
    프로그램 시작시의 맵 크기는 변하지 않으므로 상수로 설정하고 전역에서 선언하였다.
    MAP_SIZE에 따라 다차원배열을 생성하고, 모든 요소를 0으로 초기화하였다.
    올바르게 배열이 생성되었는지 확인하고, 이를 반복문을 이용해 출력하도록 하였다.

--- cppBasics/cppBasics/Main.cpp
+++ cppBasics/cppBasics/Main.cpp
@@ -1,8 +1,33 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+const int MAP_SIZE_X = 10;
+const int MAP_SIZE_Y = 10;
+
 int main()
 {
-	
+	int Map[MAP_SIZE_X][MAP_SIZE_Y] = { 0 };
+	for (int i = 0; i < MAP_SIZE_X; i++)
+	{
+		for (int j = 0; j < MAP_SIZE_Y; j++)
+		{
+			Map[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < MAP_SIZE_X; i++)
+	{
+		for (int j = 0; j < MAP_SIZE_Y; j++)
+		{
+			cout << Map[i][j] << "  ";
+			if (j == MAP_SIZE_Y - 1)
+			{
+				cout << endl;
+				cout << endl;
+			}
+		}
+	}
 
 	return 0;
 }
