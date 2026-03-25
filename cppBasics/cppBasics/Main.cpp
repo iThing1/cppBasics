e490034373806e79cdc3312863f8f41179e1d4b8
iThing1 <r930324@gmail.com>
2026-03-25 16:37:23 +0900

    [update]보물찾기 로직 설계
    플레이어의 입력값에 따라 map배열의 인덱스를 확인할 수 있는 로직을 구성하였다.
    map의 X,Y크기에 맞게 잘못된 입력값을 처리할 수 있도록 예외처리 구문을 작성하였다.
    보물을 찾기 전까지 위 과정을 반복할 수 있도록 while문을 사용하였다.
    보물을 찾은 경우 break를 통해 빠져올 수 있도록 하였다.
    몬스터 조우시 게임이 종료되도록 하였다.

--- cppBasics/cppBasics/Main.cpp
+++ cppBasics/cppBasics/Main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <cstdlib>
+#include <windows.h>
 using namespace std;
 
 const int MAP_SIZE_X = 10;
@@ -7,7 +8,7 @@ const int MAP_SIZE_Y = 10;
 const int MAX_ITEMS = 3;
 const int MAX_MONSTERS = 5;
 
-int Map[MAP_SIZE_X][MAP_SIZE_Y] = { 0 };
+int map[MAP_SIZE_X][MAP_SIZE_Y] = { 0 };
 
 void GenerateMap()
 {
@@ -15,7 +16,7 @@ void GenerateMap()
 	{
 		for (int j = 0; j < MAP_SIZE_Y; j++)
 		{
-			Map[i][j] = 0;
+			map[i][j] = 0;
 		}
 	}
 	int itemCount = rand() % MAX_ITEMS;
@@ -24,16 +25,16 @@ void GenerateMap()
 	{
 		int x = rand() % MAP_SIZE_X;
 		int y = rand() % MAP_SIZE_Y;
-		Map[x][y] = 1;
+		map[x][y] = 1;
 	}
 	int monsterCount = rand() % MAX_MONSTERS;
 	for (int i = 0; i < monsterCount;)
 	{
 		int x = rand() % MAP_SIZE_X;
 		int y = rand() % MAP_SIZE_Y;
-		if (Map[x][y] == 0)
+		if (map[x][y] == 0)
 		{
-			Map[x][y] = 2;
+			map[x][y] = 2;
 			i++;
 		}
 	}
@@ -45,7 +46,7 @@ void PrintMap()
 	{
 		for (int j = 0; j < MAP_SIZE_Y; j++)
 		{
-			cout << Map[i][j] << "  ";
+			cout << map[i][j] << "  ";
 			if (j == MAP_SIZE_Y - 1)
 			{
 				cout << endl;
@@ -54,12 +55,68 @@ void PrintMap()
 		}
 	}
 }
+
+void PrintGameOver()
+{
+	system("cls");
+	cout << "======================" << endl;
+	cout << "      Game Over      " << endl;
+	cout << "======================" << endl;
+	cin.get();
+}
+
+void PlayGame()
+{
+	int playerX = -1;
+	int playerY = -1;
+	while (true)
+	{
+		PrintMap();
+		cout << "보물을 찾을 행을 입력하세요: " << endl;
+		cin >> playerX;
+		if (playerX < 0 || playerX >= MAP_SIZE_X)
+		{
+			cout << "잘못된 입력입니다. 0에서 " << MAP_SIZE_X - 1 << " 사이의 값을 입력하세요." << endl;
+			continue;
+		}
+		cout << "보물을 찾을 열을 입력하세요: " << endl;
+		cin >> playerY;
+		if (playerY < 0 || playerY >= MAP_SIZE_Y)
+		{
+			cout << "잘못된 입력입니다. 0에서 " << MAP_SIZE_Y - 1 << " 사이의 값을 입력하세요." << endl;
+			continue;
+		}
+		cout << "보물을 찾을 좌표: (" << playerX << ", " << playerY << ")" << endl;
+		if (map[playerX][playerY] == 1)
+		{
+			cout << "보물을 획득했습니다!!!" << endl;
+			break;
+		}
+		else if (map[playerX][playerY] == 2)
+		{
+			cout << "몬스터와 조우했습니다!" << endl;
+			Sleep(800);
+			cout << "몬스터가 당신에게 달려듭니다!!" << endl;
+			Sleep(800);
+			cout << "당신은 몬스터에게 잡혔습니다..." << endl;
+			Sleep(800);
+			PrintGameOver();
+			break;
+		}
+		else
+		{
+			cout << "빈 공간입니다..." << endl;
+			Sleep(800);
+			system("cls");
+		}
+	}
+}
+
 int main()
-{	
+{
 	srand(static_cast<unsigned int>(time(NULL)));
-	
 	GenerateMap();
-	PrintMap();
+	PlayGame();
 
 	return 0;
 }
