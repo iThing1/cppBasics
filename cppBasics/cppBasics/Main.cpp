8fcc849e3d55d924cd1767bb5bc8bbc83f1b5de1
iThing1 <r930324@gmail.com>
2026-03-25 15:40:38 +0900

    [......]
    Main안의 내용을 함수로 정리함

--- cppBasics/cppBasics/Main.cpp
+++ cppBasics/cppBasics/Main.cpp
@@ -6,10 +6,8 @@ const int MAP_SIZE_X = 10;
 const int MAP_SIZE_Y = 10;
 const int MAX_ITEMS = 3;
 
-int main()
-{	
-	srand(static_cast<unsigned int>(time(NULL)));
-	int Map[MAP_SIZE_X][MAP_SIZE_Y] = { 0 };
+void GenerateMap(int Map[MAP_SIZE_X][MAP_SIZE_Y])
+{
 	for (int i = 0; i < MAP_SIZE_X; i++)
 	{
 		for (int j = 0; j < MAP_SIZE_Y; j++)
@@ -17,7 +15,6 @@ int main()
 			Map[i][j] = 0;
 		}
 	}
-
 	int itemCount = rand() % MAX_ITEMS;
 	if (itemCount == 0) itemCount = 1;
 	for (int i = 0; i < itemCount; i++)
@@ -26,7 +23,10 @@ int main()
 		int y = rand() % MAP_SIZE_Y;
 		Map[x][y] = 1;
 	}
+}
 
+void PrintMap(int Map[MAP_SIZE_X][MAP_SIZE_Y])
+{
 	for (int i = 0; i < MAP_SIZE_X; i++)
 	{
 		for (int j = 0; j < MAP_SIZE_Y; j++)
@@ -39,6 +39,14 @@ int main()
 			}
 		}
 	}
+}
+int main()
+{	
+	srand(static_cast<unsigned int>(time(NULL)));
+	int Map[MAP_SIZE_X][MAP_SIZE_Y] = { 0 };
+	
+	GenerateMap(Map);
+	PrintMap(Map);
 
 	return 0;
 }
